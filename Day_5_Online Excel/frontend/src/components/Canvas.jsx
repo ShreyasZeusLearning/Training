a2ae8723bdde6f991fd2a6d1867748120db6a69b
ShreyasKeni.ZeusLearning <shreyas.keni@zeuslearning.com>
2024-07-08 16:24:39 +0530

    Resizing

--- Day_5_Online Excel/frontend/src/components/Canvas.jsx
+++ Day_5_Online Excel/frontend/src/components/Canvas.jsx
@@ -7,6 +7,7 @@ const Canvas = () => {
 
   let canva, ctx, w, h;
   var mouseEvent = false;
+  let updateCell = false;
 
   useEffect(() => {
     canva = canvasRef.current;
@@ -148,40 +149,262 @@ const Canvas = () => {
       4900,
       5900,
     ],
+    [
+      "user1@example.com",
+      "John Doe",
+      "USA",
+      "California",
+      "Los Angeles",
+      "+1-123-456-7890",
+      "123 Main St",
+      "Apt 101",
+      "1990-01-01",
+      1500,
+      2500,
+      3500,
+      4500,
+      5500,
+    ],
+    [
+      "user1@example.com",
+      "John Doe",
+      "USA",
+      "California",
+      "Los Angeles",
+      "+1-123-456-7890",
+      "123 Main St",
+      "Apt 101",
+      "1990-01-01",
+      1500,
+      2500,
+      3500,
+      4500,
+      5500,
+    ],
+    [
+      "user2@example.com",
+      "Jane Smith",
+      "Canada",
+      "Ontario",
+      "Toronto",
+      "+1-987-654-3210",
+      "456 Elm St",
+      "Unit 202",
+      "1985-05-15",
+      1700,
+      2700,
+      3700,
+      4700,
+      5700,
+    ],
+    [
+      "user3@example.com",
+      "Alice Johnson",
+      "UK",
+      "England",
+      "London",
+      "+44-20-1234-5678",
+      "789 Oak St",
+      "Flat 3",
+      "1982-11-30",
+      1900,
+      2900,
+      3900,
+      4900,
+      5900,
+    ],
+    [
+      "user1@example.com",
+      "John Doe",
+      "USA",
+      "California",
+      "Los Angeles",
+      "+1-123-456-7890",
+      "123 Main St",
+      "Apt 101",
+      "1990-01-01",
+      1500,
+      2500,
+      3500,
+      4500,
+      5500,
+    ],
+    [
+      "user2@example.com",
+      "Jane Smith",
+      "Canada",
+      "Ontario",
+      "Toronto",
+      "+1-987-654-3210",
+      "456 Elm St",
+      "Unit 202",
+      "1985-05-15",
+      1700,
+      2700,
+      3700,
+      4700,
+      5700,
+    ],
+    [
+      "user3@example.com",
+      "Alice Johnson",
+      "UK",
+      "England",
+      "London",
+      "+44-20-1234-5678",
+      "789 Oak St",
+      "Flat 3",
+      "1982-11-30",
+      1900,
+      2900,
+      3900,
+      4900,
+      5900,
+    ],
+    [
+      "user1@example.com",
+      "John Doe",
+      "USA",
+      "California",
+      "Los Angeles",
+      "+1-123-456-7890",
+      "123 Main St",
+      "Apt 101",
+      "1990-01-01",
+      1500,
+      2500,
+      3500,
+      4500,
+      5500,
+    ],
+    [
+      "user1@example.com",
+      "John Doe",
+      "USA",
+      "California",
+      "Los Angeles",
+      "+1-123-456-7890",
+      "123 Main St",
+      "Apt 101",
+      "1990-01-01",
+      1500,
+      2500,
+      3500,
+      4500,
+      5500,
+    ],
+    [
+      "user2@example.com",
+      "Jane Smith",
+      "Canada",
+      "Ontario",
+      "Toronto",
+      "+1-987-654-3210",
+      "456 Elm St",
+      "Unit 202",
+      "1985-05-15",
+      1700,
+      2700,
+      3700,
+      4700,
+      5700,
+    ],
+    [
+      "user3@example.com",
+      "Alice Johnson",
+      "UK",
+      "England",
+      "London",
+      "+44-20-1234-5678",
+      "789 Oak St",
+      "Flat 3",
+      "1982-11-30",
+      1900,
+      2900,
+      3900,
+      4900,
+      5900,
+    ],
+    [
+      "user1@example.com",
+      "John Doe",
+      "USA",
+      "California",
+      "Los Angeles",
+      "+1-123-456-7890",
+      "123 Main St",
+      "Apt 101",
+      "1990-01-01",
+      1500,
+      2500,
+      3500,
+      4500,
+      5500,
+    ],
+    [
+      "user2@example.com",
+      "Jane Smith",
+      "Canada",
+      "Ontario",
+      "Toronto",
+      "+1-987-654-3210",
+      "456 Elm St",
+      "Unit 202",
+      "1985-05-15",
+      1700,
+      2700,
+      3700,
+      4700,
+      5700,
+    ],
+    [
+      "user3@example.com",
+      "Alice Johnson",
+      "UK",
+      "England",
+      "London",
+      "+44-20-1234-5678",
+      "789 Oak St",
+      "Flat 3",
+      "1982-11-30",
+      1900,
+      2900,
+      3900,
+      4900,
+      5900,
+    ],
+    
+    
   ];
+
+  const columnWidth = [135 , 185 , 135 , 135 , 135 , 135 , 135 , 235, 135 , 135 , 135 , 135 , 135 , 135];
+
 
   let lx, ly, rx, ry;
 
   function PaintCanva(rx = -1 , ry = -1, lx = -1 , ly = -1) {
     if(canvasRef.current != undefined){
-      // console.log(canvasRef)
-      // console.log(ctx)
 
-      // console.log(canvasRef.current.width, canvasRef.current.height);
       ctx.save();
 
       ctx.fillStyle = 'rgba(255, 255, 255, 1)';
       ctx.fillRect(0, 0, canvasRef.current.width, canvasRef.current.height);
       ctx.restore();
-      // console.log("Painting");
-      
-      // console.log(rx , ry , lx, ly);
+
       
       var x = 0;
-      // console.log(w/14)
       var width = Math.max(135, 120);
       var height = 25;
 
       for (var i = 0; i < titles.length; i++) {
         ctx.save();
-        ctx.rect(x, 0, width, height);
+        ctx.rect(x, 0, columnWidth[i], height);
         ctx.clip();
-        ctx.font = "18px Arial";
+        ctx.font = "bolder 18px Quicksand";
         ctx.fillText(titles[i], x + 2, 20);
-        x += width;
+        x += columnWidth[i];
         ctx.restore();
         ctx.stroke();
-        // console.log(x);
       }
 
       x = 0;
@@ -191,32 +414,35 @@ const Canvas = () => {
           ctx.save();
           if((i >= lx && i <= rx) && ((j >= ly && j <= ry)||(ly == ry && j == ly))){
             ctx.fillStyle = "#5ec576"
-            ctx.fillRect(x, y, width, height);
-            ctx.rect(x, y, width, height);
+            ctx.fillRect(x, y, columnWidth[j], height);
+            ctx.rect(x, y, columnWidth[j], height);
 
             ctx.clip();
 
             ctx.fillStyle = "black";
-            ctx.font = "18px Arial";
+            ctx.font = "bold 18px Quicksand";
 
             ctx.fillText(data[i][j], x + 2, y + 20);
             
           }else{
-            ctx.rect(x, y, width, height);
+            ctx.rect(x, y, columnWidth[j], height);
             ctx.stroke();
             ctx.clip();
             ctx.fillStyle = "black";
-            ctx.font = "18px Arial";
+            ctx.font = "700 18px Quicksand";
             ctx.fillText(data[i][j], x + 2, y + 20);
           }
           if(lx ==rx && ly ==ry){
             if(updateText.current != undefined && i ==lx && j == ly){
+              updateText.current.value = data[i][j];
               updateText.current.style.width = "135" + "px";
               updateText.current.style.height = "25" + "px"
               updateText.current.style.background = "white";
               updateText.current.style.left = x + "px";
               updateText.current.style.top = y + "px";
               updateText.current.style.display = "block";
+              updateText.current.focus();
+              updateCell = true;
             }
           }
           else{
@@ -225,8 +451,7 @@ const Canvas = () => {
               updateText.current.style.display = "none";
 
           }
-          // console.log(clickopt)
-          x += width;
+          x += columnWidth[j];
           ctx.restore();
         }
         y += height;
@@ -236,72 +461,78 @@ const Canvas = () => {
     }
   }
 
-  let rangeSelected = true;
-  // window.removeEventListener("mousemove", getCord);
-
-
-  // if(rangeSelected){
-  document.body.addEventListener("mouseup", (event) => {
-    console.log("MouseUp")
-    console.log(event)
-    lx = event.offsetX;
-    ly = Math.max(event.offsetY , event.clientY);
-    console.log(lx + " " + ly);
-    console.log(rx + " " + ry);
-
-    var rcolumn = Math.max(Math.floor(lx / 135) , 0);
-    var rrow = Math.max(Math.floor(ly / 25) - 1 , 0);
-    var lcolumn = Math.max(Math.floor(rx / 135) , 0);
-    var lrow = Math.max(Math.floor(ry / 25) - 1 , 0);
-    mouseEvent = false;
-    console.log(lcolumn, lrow, rcolumn, rrow , data.length , data[0].length);
-
-    if(lcolumn > rcolumn){
-      var c = lcolumn;
-      lcolumn = rcolumn;
-      rcolumn = c;
-    }
-
-    
-    if(lrow > rrow){
-      var c = lrow;
-      lrow = rrow;
-      rrow = c;
-    }
-    console.log("Event Removed");
-
-    if(rcolumn >= data[0].length || lrow >= data.length || lcolumn >= data[0].length || rrow >= data.length){
-      PaintCanva(-1 , -1 , -1 , -1)
-    }
-    else{
-      PaintCanva(rrow , rcolumn , lrow , lcolumn)
-
-      for (var i = lrow; i <= rrow; i++) {
-        console.log(data[i][rcolumn]);
-      }
-    }
-
-  });
-
-  document.body.addEventListener("mousedown", (event) => {
-    rx = event.offsetX;
-    ry = Math.max(event.offsetY , event.clientY);
-    console.log("Event" , event.x , rx , ry);
-    mouseEvent = true;
-    window.addEventListener("mousemove" ,(event) => {
-      getCord(event , rx , ry);
-    });
-  });
-
-  // function getCord(rx , ry)
-  function getCord(event , rx , ry){
-    if(mouseEvent == true){     
-      console.log("Inside MouseMove" + (rx , ry , lx , ly)); 
-      var rcolumn = Math.max(Math.floor(event.offsetX / 135) , 0);
-      var rrow = Math.max(Math.floor(event.offsetY / 25) - 1 , 0);
+  function getColAndRow(lx, ly , rx , ry){
+      var rcolumn = Math.max(Math.floor(lx / 135) , 0);
+      var rrow = Math.max(Math.floor(ly / 25) - 1 , 0);
       var lcolumn = Math.max(Math.floor(rx / 135) , 0);
       var lrow = Math.max(Math.floor(ry / 25) - 1 , 0);
 
+      return {rcolumn , rrow , lcolumn , lrow};
+  }
+
+  function getClickedDown(event) {
+    if(event.target == updateText.current){
+      return ;
+    }
+    rx = event.nativeEvent.offsetX;
+    ry = Math.max(event.nativeEvent.offsetY , event.nativeEvent.clientY);
+    // console.log("Event" , event , rx , ry);
+    mouseEvent = true;
+    
+    
+    canvasRef.current.addEventListener("pointermove" ,getCord);
+
+    
+    function getCord(event){
+      // if(mouseEvent == true){     
+        // console.log("Inside MouseMove" + (rx , ry , lx , ly)); 
+        var rcolumn = Math.max(Math.floor(event.offsetX / 135) , 0);
+        var rrow = Math.max(Math.floor(event.offsetY / 25) - 1 , 0);
+        var lcolumn = Math.max(Math.floor(rx / 135) , 0);
+        var lrow = Math.max(Math.floor(ry / 25) - 1 , 0);
+  
+        if(lcolumn > rcolumn){
+          var c = lcolumn;
+          lcolumn = rcolumn;
+          rcolumn = c;
+        }
+    
+        
+        if(lrow > rrow){
+          var c = lrow;
+          lrow = rrow;
+          rrow = c;
+        }
+  
+        // console.log("<MouseOver :- " + (lrow , lcolumn , rrow , rcolumn))
+  
+        PaintCanva(rrow , rcolumn , lrow , lcolumn);
+      // }
+    }
+
+    canvasRef.current.addEventListener("pointerup", (event) => {
+      canvasRef.current.removeEventListener("pointermove", getCord);
+      console.log("Mouse Uped")
+  
+      if(event.target == updateText.current){
+        return ;
+      }
+      // console.log("MouseUp")
+      // console.log(event)
+      lx = event.offsetX;
+      ly = Math.max(event.offsetY , event.clientY);
+      // console.log(event.offsetX + " " + ly);
+      // console.log(rx + " " + ry);
+  
+      // var rcolumn = Math.max(Math.floor(lx / 135) , 0);
+      // var rrow = Math.max(Math.floor(ly / 25) - 1 , 0);
+      // var lcolumn = Math.max(Math.floor(rx / 135) , 0);
+      // var lrow = Math.max(Math.floor(ry / 25) - 1 , 0);
+      var {rrow , rcolumn , lrow , lcolumn} = getColAndRow(lx , ly , rx , ry);
+
+      // mouseEvent = false;
+      // console.log(rcolumn, rrow);
+  
       if(lcolumn > rcolumn){
         var c = lcolumn;
         lcolumn = rcolumn;
@@ -314,16 +545,55 @@ const Canvas = () => {
         lrow = rrow;
         rrow = c;
       }
+      // console.log("Event Removed");
+  
+      if(rcolumn >= data[0].length || lrow >= data.length || lcolumn >= data[0].length || rrow >= data.length){
+        PaintCanva(-1 , -1 , -1 , -1)
+        if(updateCell == true)
+          updateText.current.style.display = "none"
+      }
+      else{
+        PaintCanva(rrow , rcolumn , lrow , lcolumn)
+  
+        // for (var i = lrow; i <= rrow; i++) {
+        //   console.log(data[i][rcolumn]);
+        // }
+      }
 
-      console.log("<MouseOver :- " + (lrow , lcolumn , rrow , rcolumn))
+      document.body.addEventListener('keyup' , PressEnter);
 
-      PaintCanva(rrow , rcolumn , lrow , lcolumn);
-    }
-  }
-  // }
+      function PressEnter(event){
+        // console.log(event)
+        if(event.key == 'Enter' && updateCell){
+          // alert('Enter is pressed!');
+          // console.log(event)
+          var {rrow , rcolumn , lrow , lcolumn} = getColAndRow(lx , ly , rx , ry);
+          console.log(rrow , rcolumn , lrow , lcolumn);
+          // console.log(event.target.value);
+          // console.log(data[rrow][rcolumn]);
+          data[rrow][rcolumn] = event.target.value;
+          updateCell = false;
+          updateText.current.style.display = "none";
+          PaintCanva(-1 , -1 , -1 , -1);
+          // console.log(event)
+        }
+      }
+    });
+  };
+
+  //  function PressEnter(event){
+    // console.log(event)
+    // if(event.key == 'Enter'){
+    //   // alert('Enter is pressed!');
+    // // console.log(rrow , rcolumn);
+    // console.log(event)
+    // }
+
+  // };
+
   return (
     <div className="canva-bg">
-      <canvas ref={canvasRef} width="1902px" height="400px"></canvas>
+      <canvas ref={canvasRef} onPointerDown={(event) => getClickedDown(event)} width="1902px" height="1000px"></canvas>
       <input type="text" ref={updateText} name="" className="inputText" />
     </div>
   );
